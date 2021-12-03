efdacd484814f7b0f0d03750c511dc966c1f4b16
Wilson Cheah <wilsoncheah5@gmail.com>
2021-12-03 15:10:18 -0800

    commented out test

--- Day03-2.js
+++ Day03-2.js
@@ -56,18 +56,18 @@ const verifyLifeSupport = (d) => {
     return parseInt(oxygen.join(''), 2) * parseInt(co2.join(''), 2);
 };
 
-console.log(verifyLifeSupport([
-    '00100',
-    '11110',
-    '10110',
-    '10111',
-    '10101',
-    '01111',
-    '00111',
-    '11100',
-    '10000',
-    '11001',
-    '00010',
-    '01010',
-]));
+// console.log(verifyLifeSupport([
+//     '00100',
+//     '11110',
+//     '10110',
+//     '10111',
+//     '10101',
+//     '01111',
+//     '00111',
+//     '11100',
+//     '10000',
+//     '11001',
+//     '00010',
+//     '01010',
+// ]));
 console.log(verifyLifeSupport(diagnostic));
